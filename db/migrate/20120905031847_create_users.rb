f261bb330a2fda734cc73269e6fd0f0917980e58
Nuttapon <nuttapon@code-app.com>
2012-10-17 10:50:43 +0700

    init

--- db/migrate/20120905031847_create_users.rb
+++ db/migrate/20120905031847_create_users.rb
@@ -6,6 +6,6 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
-    User.create!(:fullname => "Admin",:password => "mefuwfhfu", :password_confirmation => "mefuwfhfu" :email => "admin@gmail.com", :admin => true)
+    User.create!(:fullname => "Admin",:password => "mefuwfhfu", :password_confirmation => "mefuwfhfu" ,:email => "admin@gmail.com", :admin => true)
   end
 end
